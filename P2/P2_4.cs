0fe8b5d6b45734f456a3b49dfb578840297927e4
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2019-01-29 11:38:37 +0530

    29/1/19

---
+++ P2/P2_4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class P2_4 
+{
+	public P2_4()
+	{
+	}
+
+    public static void Main()
+    {
+        Maruti m1 = new Maruti("maruti",1,"top");
+        Car c = new Car();
+        m1.display();
+    }
+}
+
+public class Car
+{
+   public Car() { }
+    protected int id;
+    protected string name;
+    private string model;
+
+    public Car(string model)
+    {
+        this.model = model;
+    }
+    public void display()
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(id);
+        Console.WriteLine(model);
+    }
+}
+
+public class Maruti : Car
+{
+    public Maruti() {
+        //id = 1;
+        //name = "maruti";
+    }
+    public Maruti(String name, int id, string model) : base(model)
+    {
+        this.name = name;
+        this.id = id;
+        
+    }
+}
+
+public class Mahindra : Car
+{
+    public Mahindra() {
+
+    }
+}
---
+++ P3/MethodOverloading.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class MethodOverloading
+{
+    public int add(int i, int j)
+    {
+        return i + j;
+    }
+    public Vector add(Vector v1, Vector v2)
+    {
+        Vector temp = new Vector();
+        temp.i = v1.i + v2.i;
+        return temp;
+    }
+    public void add(Matrix m1, Matrix m2)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+            }
+        }
+    }
+    public MethodOverloading();
+    public static void Main()
+    {
+        int a = 5, b = 10;
+        Vector x = new Vector(20);
+        Vector y = new Vector(30);
+        Vector z;
+
+        int  intg = add(a, b);
+        Console.WriteLine(intg);
+
+        z = add(x, y);
+        Console.WriteLine(z.ToString());
+            
+    }
+}
+public class Vector
+{
+    int i;
+    public Vector();
+    public Vector(int i)
+    {
+        this.i = i;
+    }
+}
+/*
+public class Matrix
+{
+    int[,] matx = new int[3, 3];
+    int[,] maty = new int[3, 3];
+    public Matrix();
+    public Matrix(int[,] matx, int[,] maty)
+    {
+        this.matx = matx;
+        this.maty = maty;
+    }
+}*/

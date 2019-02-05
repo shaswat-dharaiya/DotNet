d0f4992ed1960b9241f85d77cd3b7581569e3a58
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2019-02-05 10:53:29 +0530

    P3

--- P3/MethodOverloading.cs
+++ P3/MethodOverloading.cs
@@ -2,26 +2,28 @@
 
 public class MethodOverloading
 {
-    public int add(int i, int j)
+    public static int add(int i, int j)
     {
         return i + j;
     }
-    public Vector add(Vector v1, Vector v2)
+    public static Vector add(Vector v1, Vector v2)
     {
         Vector temp = new Vector();
         temp.i = v1.i + v2.i;
         return temp;
     }
-    public void add(Matrix m1, Matrix m2)
+    public static Matrix add(Matrix m1, Matrix m2)
     {
+        Matrix temp = new Matrix();
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
+                temp.mat[i,j] = m1.mat[i, j] + m2.mat[i, j];
             }
         }
+        return temp;
     }
-    public MethodOverloading();
     public static void Main()
     {
         int a = 5, b = 10;
@@ -29,32 +31,65 @@ public class MethodOverloading
         Vector y = new Vector(30);
         Vector z;
 
+        int[,] a1 = new int[3, 3];
+        int[,] a2 = new int[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                a1[i, j] = i + j;
+                a2[j, i] = i + j;
+            }
+        }
+        Matrix mat1 = new Matrix(a1);
+        Matrix mat2 = new Matrix(a2);
+        Matrix mat = new Matrix();
+
         int  intg = add(a, b);
         Console.WriteLine(intg);
 
         z = add(x, y);
         Console.WriteLine(z.ToString());
+
+        mat = add(mat1, mat2);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(mat.mat[i,j].ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+        
             
     }
 }
 public class Vector
 {
-    int i;
-    public Vector();
+    public int i;
+    public Vector() { }
     public Vector(int i)
     {
         this.i = i;
     }
+    
+    public override String ToString()
+    {
+        return i.ToString();
+    }
 }
-/*
+
 public class Matrix
 {
-    int[,] matx = new int[3, 3];
-    int[,] maty = new int[3, 3];
-    public Matrix();
-    public Matrix(int[,] matx, int[,] maty)
+    public int[,] mat = new int[3, 3];
+    public Matrix() { }
+    public Matrix(int[,] mat)
     {
-        this.matx = matx;
-        this.maty = maty;
+        this.mat = mat;
     }
-}*/
+
+    public override string ToString()
+    {
+        return mat.ToString();
+    }
+}
---
+++ P3/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Student
+{
+    public string name;
+    public long enroll;
+    public string branch;
+
+	public Student(){}
+
+    public Student(string name)
+    {
+        this.name = name;
+    }
+    public Student(string name, long enroll)
+    {
+        this.name = name;
+        this.enroll = enroll;
+    }
+    public Student(string name, long enroll, string branch)
+    {
+        this.name = name;
+        this.enroll = enroll;
+        this.branch = branch;
+    }
+
+    public static void Main()
+    {
+        string name, branch;
+        long enroll;
+
+        name = "Shaswat";
+        enroll = 160470107052;
+        branch = "CE";
+
+        Student s1 = new Student(name);
+        Student s2 = new Student(name,enroll);
+        Student s3 = new Student(name, enroll,branch);
+        s1.disp();
+        s2.disp();
+        s3.disp();
+    }
+
+    public void disp()
+    {
+        Console.WriteLine(this.name);
+        Console.WriteLine(this.enroll);
+        Console.WriteLine(this.branch);
+    }
+}
